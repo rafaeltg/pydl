0989b3402e307509d866c7ddd8f90dfd464e34fa
rafaeltg <rthomazigonzalez@gmail.com>
2017-03-24 01:57:10 -0300

    .

--- cli/commands/score.py
+++ cli/commands/score.py
@@ -1,37 +1,23 @@
 import os
-from .utils import get_model, get_input_data, load_data
-from pydl.models import SupervisedModel
-from pydl.model_selection import available_metrics
-from pydl.models.utils.utilities import load_model, save_json
+from .utils import get_input_data, load_data
+from pydl.model_selection import get_scorer
+from pydl.models.utils.utilities import save_json
 
 
 def score(config, output):
     """
     """
 
-    m = load_model(get_model(config))
+    assert 'scoring' in config, 'Missing scoring!'
+    scoring = config['scoring'] if 'scoring' in config else []
 
     data_set = get_input_data(config)
-    x = load_data(data_set, 'data_x')
+    actual_out = load_data(data_set, 'actual_out')
+    pred_out = load_data(data_set, 'predict_out')
 
-    metrics = config['metrics'] if 'metrics' in config else []
+    results = dict([(s, get_scorer(s)(actual_out, pred_out)) for s in scoring])
 
-    results = {}
-    if isinstance(m, SupervisedModel):
-        y = load_data(data_set, 'data_y')
-
-        results[m.get_loss_func()] = m.score(x, y)
-        if len(metrics) > 0:
-            y_pred = m.predict(x)
-            for metric in metrics:
-                results[metric] = available_metrics[metric](y, y_pred)
-
-    else:
-        results[m.get_loss_func()] = m.score(x)
-        if len(metrics) > 0:
-            x_rec = m.reconstruct(m.transform(x))
-            for metric in metrics:
-                results[metric] = available_metrics[metric](x, x_rec)
+    out_file = config['out_file'] if 'out_file' in config else 'scores.json'
 
     # Save results into a JSON file
-    save_json(results, os.path.join(output, m.name+'_scores.json'))
+    save_json(results, os.path.join(output, out_file))
