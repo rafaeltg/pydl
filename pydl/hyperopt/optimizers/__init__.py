1a8f3572099f8af1c452db7610bf4b5dd15c50db
rafaelg <rafaelg@springtechcapital.com>
2020-05-28 23:59:38 -0300

    Adding hyperopt load optimizer from config

--- pydl/hyperopt/optimizers/__init__.py
+++ pydl/hyperopt/optimizers/__init__.py
@@ -1,2 +1,27 @@
 from .optimizer import Optimizer
 from .cmaes import CMAES
+
+
+_objs = {
+    CMAES.__name__: CMAES
+}
+
+
+def optimizer_from_config(config: dict) -> [Optimizer]:
+    if len(config) == 0:
+        return None
+
+    cls_name = config.get("class_name", None)
+    if cls_name is None:
+        return None
+
+    cls = _objs.get(cls_name, None)
+    if cls is None:
+        return None
+
+    cfg = config.get("config", dict())
+
+    if len(cfg) > 0 and hasattr(cls, "from_config"):
+        return cls.from_config(**cfg)
+
+    return cls(**cfg)
